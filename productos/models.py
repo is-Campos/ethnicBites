c314e0e25959ba1391d8329355c3763eade11441
Iván Campos <ivano.cs89@gmail.com>
2023-11-20 18:50:45 -0600

    users can now register, login and signout, prodcuts can be added and their images are displayed on product detail page

--- productos/models.py
+++ productos/models.py
@@ -8,7 +8,8 @@ class Producto(models.Model):
       nombre = models.CharField(max_length=100)
       descripcion = models.CharField(max_length=1000)
       precio = models.FloatField(validators=[MinValueValidator(limit_value=1)])
-      stock = models.IntegerField(validators=[MinValueValidator(limit_value=0)])
+      stock = models.IntegerField(null=True, validators=[MinValueValidator(limit_value=0)])
       idVendedor = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
       tipo = models.CharField(max_length=100)
+      imagen = models.ImageField(upload_to='productos/files/imagenesProductos')
       
