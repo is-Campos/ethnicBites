a9adc0649bd44eda20dd7f1fbb2800c2b369e179
Diego Juárez <{ID}+{username}@users.noreply.github.com>
2023-12-04 16:31:40 -0600

    Minor fixes

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
   path("registro/", views.registro, name="registro"),
   path('logout/', views.cerrarsesion, name='cerrarsesion'),
   path('iniciarsesion/', views.iniciarsesion, name='iniciarsesion'),
-  path('direccion/', views.direccion , name='direccion')
+  path('direccion/', views.direccion , name='direccion'),
   path('admin/', views.adminHome, name="adminHome"),
   path('vendedor/', views.vendedorHome, name="vendedorHome"),
-  path('direccion/', views.direccion , name='direccion')
   path('vendedor/modificarproducto/<int:pk>', views.modificarProducto, name="modificarproducto"),
   path('vendedor/<int:pk>/delete/', views.deleteProduct, name='deleteProduct')
 ]
